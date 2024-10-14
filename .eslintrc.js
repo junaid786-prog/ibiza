6819afcf57e8428a8fef575ae4b5fabfd6c733fe
junaid786-prog <junaidshoaib555@gmail.com>
2024-10-14 13:33:12 +0500

    eslint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,16 @@ module.exports = {
         args: 'after-used',
         caughtErrors: 'none',
         ignoreRestSiblings: true,
-        vars: 'all'
-      }
+        vars: 'all',
+      },
     ],
     'prefer-const': 'error',
-    'react-hooks/exhaustive-deps': 'error', 
-  }
+    'react-hooks/exhaustive-deps': 'error',
+    // Additional rules:
+    'jsx-a11y/anchor-is-valid': 'off', // Ensures Link components are used instead of <a> tags for navigation
+    'react/jsx-pascal-case': 'error', // Enforces PascalCase for component naming
+    'react/react-in-jsx-scope': 'off', // Not needed for Next.js projects since React is automatically in scope
+    'no-console': ['warn', { allow: ['warn', 'error'] }], // Allows warnings and errors, but gives a warning for other console uses
+    'react/jsx-no-target-blank': ['error', { enforceDynamicLinks: 'always' }], // Ensures security for external links
+  },
 };
